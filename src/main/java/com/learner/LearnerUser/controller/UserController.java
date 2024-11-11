af9a632f378d5bbd3db833ba3c4ac7c09ba0b9ed
subhanicool01 <subhanicool01@gmail.com>
2024-11-11 17:19:44 +0530

    Update UserController.java

--- src/main/java/com/learner/LearnerUser/controller/UserController.java
+++ src/main/java/com/learner/LearnerUser/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
         return this.userRepository.findAll();
     }
 
-    @CrossOrigin(origins = "*")
+    //@CrossOrigin(origins = "*")
     @GetMapping("/getUserById")
     public User getUserByUserId(@RequestParam long userId, HttpServletResponse response) {
         User user = this.userRepository.findById(userId);
